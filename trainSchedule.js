129a4c949ea2cc4574ed60c5883a338d354abf89
shaunsnowden <ssnowden07@gmail.com>
2017-09-05 18:57:35 -0600

    update table

--- trainSchedule.js
+++ trainSchedule.js
@@ -16,8 +16,7 @@ $('#addTrainBtn').on('click',function(evt) {
 	evt.preventDefault();
 	var trainName = $('#inputTrain').val().trim();
 	var destination = $('#inputDestination').val().trim();
-	var firstTime = $('#inputFirstTime').val().trim();
-	// .format("hh:mm");
+	var firstTime = moment($('#inputFirstTime').val().trim(), "hh:mm").format("hh:mm");
 	var frequency = $('#inputFrequency').val().trim();
 
 	var newTrain = {
@@ -31,5 +30,31 @@ $('#addTrainBtn').on('click',function(evt) {
 
 	// uploads object to database
 	database.ref().push(newTrain);
+
+	// clear all text boxes
+	$('#inputTrain').val("");
+	$('#inputDestination').val("");
+	$('#inputFirstTime').val("");
+	$('#inputFrequency').val("");
+
+});
+
+// Push new entry to the html
+database.ref().on("child_added",function(childSnapshot) {
+	var trainName = childSnapshot.val().name;
+	var destination = childSnapshot.val().place;
+	var firstTrain = childSnapshot.val().firsttrain;
+	var frequency = childSnapshot.val().freq;
+	var trainNumber = 1;
+
+	var currentTime = moment().format("hh:mm");
+	var timeDiff = moment().diff(moment(firstTrain), "minutes");
+	var timeRemainder = timeDiff % frequency;
+	var minToTrain = frequency - timeRemainder;
+	var nxtTrain = moment().add(minToTrain, "minutes").format("hh:mm");
+
+	$('#trainTable').append("<tr><td>" + trainNumber + "</td><td>"+ trainName + "</td><td>" + destination + "</td><td>" + nxtTrain + "</td><td>" + frequency + "</td><td>" + minToTrain + "</td></tr>");
+
+	trainNumber++;
 
 });
